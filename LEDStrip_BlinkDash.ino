06d6abfeb7a99603af7fc838ccf405734728b27a
winnam <89352294+winnam@users.noreply.github.com>
2023-04-02 16:40:56 -0400

    Update LEDStrip_BlinkDash.ino

--- LEDStrip_BlinkDash.ino
+++ LEDStrip_BlinkDash.ino
@@ -11,13 +11,11 @@ void setup() {
 
  
 }
-int colors[] = {0xCCCC00, 0x1111CC, 0x003333, 0x882222};
 void loop() {
   // put your main code here, to run repeatedly:
   pixels.fill(0, 0, 10);
   for (int i = 0; i < 10; i++) {
     if(i > 0) pixels.fill(0,i-1,1);
-    //pixels.rainbow(0,0,255,255);
     pixels.fill(0x222200,i, 1);
     delay(delayVal);
     pixels.show();
@@ -25,7 +23,6 @@ void loop() {
   for (int i = 9; i >= 0; i--) {
     if(i < 9) pixels.fill(0,i+1,1);
     pixels.fill(0x005555,i, 1);
-    //pixels.rainbow(0,1,255,255);
     delay(delayVal);
     pixels.show();
   }
